a706e31608c4338ce30720d47d9705bd0dfe50cd
German Kosach <fuerstein@yandex.ru>
2023-10-06 19:48:28 +0300

    prisma bugs

--- prisma/prisma-client.js
+++ prisma/prisma-client.js
@@ -1,12 +1,8 @@
-import { PrismaClient, User, Employee } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 const user = await prisma.user.findUnique({ where: { id: 'someId' } });
 const employees = await prisma.employee.findMany({ where: { userId: 'someUserId' } });
 
-export default {
-    prisma,
-    user,
-    employees
-};
+export { prisma, user, employees };
