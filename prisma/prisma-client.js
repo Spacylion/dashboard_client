c1c3ec8e0cee0aeddda25e41e9742ed8f7a72d77
German Kosach <fuerstein@yandex.ru>
2023-10-06 19:26:00 +0300

    bugs with prismz

--- prisma/prisma-client.js
+++ prisma/prisma-client.js
@@ -1,8 +1,12 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, User, Employee } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 const user = await prisma.user.findUnique({ where: { id: 'someId' } });
 const employees = await prisma.employee.findMany({ where: { userId: 'someUserId' } });
 
-export { prisma, user, employees };
+export default {
+    prisma,
+    user,
+    employees
+};
